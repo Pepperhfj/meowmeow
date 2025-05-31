0d5996744883a0724e4e84bf3b05cf989ee10991
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-05-31 16:50:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-button.onclick = increaseValue;
+button.onclick = increaseValue; // Clicking the button will activate the increaseValue function !
 
 function increaseValue() {
-  var value = document.getElementById('meow');
-  value.innerHTML +=" meow !";
+  var value = document.getElementById('meow'); // Get the ID of 'meow' and assigns it as a variable >_<
+  value.innerHTML +=" meow !"; // Increases the 'value' amount in the HTML ^o^
 }
